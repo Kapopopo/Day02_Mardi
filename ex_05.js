7a6ac130740c55fff044573fe8a2a7b6d6d0e991
kapopay <nawfal.yessoufou@epitech.eu>
2024-10-01 14:54:53 +0200

    finish

--- ex_05.js
+++ ex_05.js
@@ -1,15 +1,17 @@
 let array = ["strawberry" , "strawberry" , "strawberry" ,
-"apple" , "apple" ,
-"lime" , 
-"lime" , "peach" , "pear" ,
-"pear"];
+	     "apple" , "apple" ,
+	     "lime" , 
+	     "lime" , "peach" , "pear" ,
+	     "pear"];
 
 function getBasketContent() {
-    return array;
+    let basket = array;
+    console.log(basket.length + "fruit(s) selected");
+    return basket;
 }
 
 // DO NOT TOUCH, this will do the call your function for you and display the fruits if you've done it well
 if (typeof getBasketContent === "function") {
-    const fruits = getBasketContent()
-    showMyBasket(fruits)
+	const fruits = getBasketContent()
+	showMyBasket(fruits)
 }
